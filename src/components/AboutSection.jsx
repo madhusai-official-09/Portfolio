e1a530a2473ca88a76d9f0dc40876bc1adae9765
Pitani Madhusayi krishna Maheshwara Prakash Rao <22221a4545@gmail.com>
2025-09-29 19:42:26 +0530

    Update AboutSection.jsx

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -29,14 +29,10 @@ export const AboutSection = () => {
                         Get In Touch
                         </a>
 
-                    <a href="https://drive.google.com/file/d/1nlqwauqRYtpzrtsPJgUSXOKmoyyArDeg/view?usp=drive_link" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
-  {" "}
-  Download CV
-</a>
-
-                        {" "}
-                        Download CV
-                        </a>
+         <a href="https://drive.google.com/file/d/1nlqwauqRYtpzrtsPJgUSXOKmoyyArDeg/view?usp=drive_link" className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+          {" "}
+          Download CV
+            </a>
                 </div>
             </div>
 
